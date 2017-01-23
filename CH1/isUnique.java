ffba092674a3c4c9e39182d3f9ab5bd3df0b2151
Albert Hermida <alberthermida@gmail.com>
2017-01-22 22:27:25 -0500

    got it working

--- CH1/isUnique.java
+++ CH1/isUnique.java
@@ -1,9 +1,22 @@
+import java.util.*;
+
 public class isUnique {
     public static void main(String[] args) {
         System.out.println(unique("yello"));
+        System.out.println(unique("Yoghurt"));
+        System.out.println(unique("Why"));
+        System.out.println(unique("Scruble"));
     }
     //determine whether all characters in the string are unique
     public static boolean unique(String s) {
-        return false;
+        HashSet<Character> seen = new HashSet<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            if (seen.contains(s.charAt(i))) {
+                return false;
+            } else {
+                seen.add(s.charAt(i));
+            }
+        }
+       return true; 
     }
 }
