580c815ec839f99dc820d3aae932d65255b83f37
Albert Hermida <alberthermida@gmail.com>
2017-01-22 22:10:37 -0500

    gitignore compiled programs

---
+++ CH1/isUnique.java
@@ -0,0 +1,9 @@
+public class isUnique {
+    public static void main(String[] args) {
+        System.out.println(unique("yello"));
+    }
+    //determine whether all characters in the string are unique
+    public static boolean unique(String s) {
+        return false;
+    }
+}
